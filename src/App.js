d8b2f9bfe45893310bb8f6946374a0104b612c07
Alex Leon <alejandroleon5434@gmail.com>
2021-09-16 14:27:16 -0700

    sandbox checkout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
-import { getAnimeOne, getAnimeTwo, getAnimeThree } from './utils/axiosRequest'
-
+ 
 const App = () => {
   useEffect(getAnimeOne(), [])
   // useEffect(getAnimeTwo(), [])
