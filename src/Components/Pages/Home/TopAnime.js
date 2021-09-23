4dfb4040dffde7bea0bc25e4fbfc3a0d203a0aed
Robyn Beck <deft.or.die@gmail.com>
2021-09-23 16:39:28 -0700

    added upcoming anime section

--- src/Components/Pages/Home/TopAnime.js
+++ src/Components/Pages/Home/TopAnime.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import { TopSection, TopMain, TopLeft, TopContainer, ColBox, SubBox } from './HomeStyle.js';
+import React, { useEffect, useState } from 'react'
+import {
+  TopSection,
+  TopMain,
+  TopLeft,
+  TopContainer,
+  ColBox,
+  SubBox,
+} from './HomeStyle.js'
+import { RequestOptions } from '../../../utils/index'
 
-export default function TopAnime({ topTen }) {
-	return (
-    <TopContainer>
-      {topTen?.map((info, index) => (
-        <TopSection>
-          <TopLeft></TopLeft>
-          <TopMain>
-            <ColBox>
-              <SubBox>SubBox</SubBox>
-              <SubBox>SubBox</SubBox>
-            </ColBox>
-            <ColBox>
-              <SubBox>SubBox</SubBox>
-              <SubBox>SubBox</SubBox>
-            </ColBox>
-            <ColBox>
-              <SubBox>SubBox</SubBox>
-              <SubBox>SubBox</SubBox>
-            </ColBox>
-          </TopMain>
-        </TopSection>
-      ))}
-    </TopContainer>
-  )
+export default function TopAnime() {
+  const [topAnime, setTopAnime] = useState([])
+  const { General: TopAnimeRequest } = RequestOptions
+
+  const renderTopAnime = () => {
+    topAnime?.map((anime, index) => (
+      <TopSection key={index}>
+        <TopLeft></TopLeft>
+        <TopMain>
+          <ColBox>
+            <SubBox>SubBox</SubBox>
+            <SubBox>SubBox</SubBox>
+          </ColBox>
+          <ColBox>
+            <SubBox>SubBox</SubBox>
+            <SubBox>SubBox</SubBox>
+          </ColBox>
+          <ColBox>
+            <SubBox>SubBox</SubBox>
+            <SubBox>SubBox</SubBox>
+          </ColBox>
+        </TopMain>
+      </TopSection>
+    ))
+  }
+
+  return <TopContainer>{}</TopContainer>
 }
