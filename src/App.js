57fa344a1ff2a8da21aa4cd2e32749acb4987587
Alex Leon <alejandroleon5434@gmail.com>
2021-09-23 14:56:20 -0700

    working branch dev

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import Global from './Styles/global';
 import Home from './Components/Pages/Home/Home';
-import Search from './Components/Pages/Search/Search';
+
 import TopAnime from './Components/Pages/Home/TopAnime';
 import TrendingAnime from './Components/Pages/Home/TrendingAnime';
 import TrendingManga from './Components/Pages/Home/TrendingManga';
 import Grid from './Components/Pages/Grid/Grid';
-const data = [
-	{ id: 'hello' },
-	{ id: 'hello' },
-	{ id: 'hello' },
-	{ id: 'hello' },
+import Navbar from './Components/Nav/Navbar';
+import styled from 'styled-components';
 
-	{ id: 'hello' },
-
-	{ id: 'hello' },
-];
+const Container = styled.div`
+	position: absolute;
+	height: 100vh;
+	width: 100vw;
+`;
 const App = () => {
 	return (
 		<>
 			<Global />
-			<Search />
-
+			<Navbar />
 			<TrendingAnime />
 			<TrendingManga />
-			<Grid id={data} />
+			<Grid />
 			<TopAnime />
 		</>
 	);
