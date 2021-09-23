e37942e7bbe0de5d14f9d03adad46132372d14b7
Robyn Beck <deft.or.die@gmail.com>
2021-09-23 11:11:22 -0700

    updated import path

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { default as Categories } from './Categories'
 export { default as Home } from './Home'
+export { default as Manga } from './Manga'
 export { default as SearchResults } from './SearchResults'
